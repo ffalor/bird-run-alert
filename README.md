5a9b2a9bcd957af5051fa1cfd056dbed8275e81c
ffalor <ffalorjr@outlook.com>
2021-01-29 22:03:00 -0600

    add bird run alert info

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Bird Run Alert
-Bird run alert for those bird runs. ALERT!
+
+This is a bird run alert for those bird runs. It simply alerts when its bird run time.
+
+Featuring fat_clouds. Check him out on his [twitch channel](https://www.twitch.tv/fat_clouds).
+
+# Requirements
+
+This plugin requires the default runelite `Time Tracking` plugin to be enabled and the `Birdouse notifiication` box to be checked.
+
+![TimeTracking](./docs/imgs/timetracking.png)
+
+# FAQ
+
+## The plugin is too loud/quite
+
+The volume of plugin is determined by your runelite volume. If you need to increase or lower the volume use the sound mixer on windows.
+
+![VolumeMixer](./docs/imgs/volumemixer.png)
+
+## The alert doesn't play
+
+Double check you have the `Time Tracking` plugin enabled and the `Birdhouse notifiication` box checked. If it still does not work please open an issue and desribe what is happening.
--- src/main/java/com/bird/runalert/BirdRunAlertPlugin.java
+++ src/main/java/com/bird/runalert/BirdRunAlertPlugin.java
@@ -2,20 +2,31 @@ package com.bird.runalert;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+import javax.sound.sampled.*;
+
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.Notifier;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.task.Schedule;
+
+import java.awt.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.temporal.ChronoUnit;
+
 
 @Slf4j
 @PluginDescriptor(
 	name = "Bird Run Alert"
 )
+
 public class BirdRunAlertPlugin extends Plugin
 {
 	@Inject
@@ -23,6 +34,9 @@ public class BirdRunAlertPlugin extends Plugin
 
 	@Inject
 	private BirdRunAlertConfig config;
+
+	@Inject
+	private  Notifier notifier;
 
 	@Override
 	protected void startUp() throws Exception
@@ -36,14 +50,32 @@ public class BirdRunAlertPlugin extends Plugin
 		log.info("Bird Run Alert stopped!");
 	}
 
+	private Clip clip = null;
+
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Bird Run Alert says " + config.greeting(), null);
+	}
+
+	@Subscribe
+	public void onChatMessage(ChatMessage message)
+	{
+		if ((message.getMessage().toLowerCase()).contains("bird houses are ready to be dismantled") && message.getType().toString().equals("CONSOLE")) {
+			InputStream fileStream = new BufferedInputStream(BirdRunAlertPlugin.class.getClassLoader().getResourceAsStream("bird_run_alert.wav"));
+			try (AudioInputStream sound = AudioSystem.getAudioInputStream(fileStream))
+			{
+				clip = AudioSystem.getClip();
+				clip.open(sound);
+				clip.setFramePosition(clip.getFrameLength());
+				clip.loop(1);
+			}
+			catch (UnsupportedAudioFileException | IOException | LineUnavailableException e)
+			{
+				log.warn("Unable to load bird run alert sound", e);
+			}
 		}
 	}
+
 
 	@Provides
 	BirdRunAlertConfig provideConfig(ConfigManager configManager)
