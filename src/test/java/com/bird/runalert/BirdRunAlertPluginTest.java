78687038e34c3ba9d924874f2d9996b967b480d1
Joris van Roy <jorisvanroy@onsmail.nl>
2022-10-21 19:40:18 +0200

    Added volume control

--- src/test/java/com/bird/runalert/BirdRunAlertPluginTest.java
+++ src/test/java/com/bird/runalert/BirdRunAlertPluginTest.java
@@ -2,6 +2,8 @@ package com.bird.runalert;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class BirdRunAlertPluginTest
 {
@@ -9,5 +11,15 @@ public class BirdRunAlertPluginTest
 	{
 		ExternalPluginManager.loadBuiltin(BirdRunAlertPlugin.class);
 		RuneLite.main(args);
+	}
+
+	@Test
+	public void getPercentileBetweenTwoNumbers() {
+		double min = 50;
+		double max = 100;
+		double percentage = 50;
+
+		double expected = 75;
+		Assert.assertEquals(expected, (percentage/max)*min + min, 0);
 	}
 }
